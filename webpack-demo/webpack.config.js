3313ae9d3a0df956cb16488431f23ee8953a3c92
mutai-gilbert <mutaigilbert85@gmail.com>
2022-12-07 08:44:19 +0300

    Learning webpack and the plugins

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+        },
+    mode: "development",
+    plugins: [
+         new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    
+};
